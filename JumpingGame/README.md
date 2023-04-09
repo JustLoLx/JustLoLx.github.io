b76c55b7aaa6bef7b90a121e185788d75e6dd73c
JustLoLx <125729073+JustLoLx@users.noreply.github.com>
2023-04-09 10:41:44 +0300

    Added new folder 'JumpingGame' in there is simple jumping game make with CSS and JS

---
+++ JumpingGame/README.md
@@ -0,0 +1,3 @@
+# This is game make by simple functions and textures with 'CSS' and 'JS' 
+
+-[HERE is the Game](https://justlolx.github.io/JumpingGame/Game.html)
---
+++ JumpingGame/script.js
@@ -0,0 +1,28 @@
+var character = document.getElementById("character");
+var block = document.getElementById("block");
+var counter = 0;
+
+
+
+function jump()
+{
+	if (character.classList=='')
+	{
+		character.classList.add("animate");
+	}
+	
+	setTimeout(function(){
+        character.classList.remove("animate");
+    },300);
+	
+}
+
+var checkDead = setInterval(function() 
+{
+    let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+    let blockLeft =   parseInt(window.getComputedStyle(block).getPropertyValue("left"));
+	//check if collision acures 
+
+	
+
+}, 10);
